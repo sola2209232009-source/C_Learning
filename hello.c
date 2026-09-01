703f5b5fb0f3b3c58b482328b3c994a7b49f0a65
sola2209232009-source <sola2209232009@gmail.com>
2026-09-01 17:21:32 +0800

    Add addition function

--- hello.c
+++ hello.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+int add(int x,int y)
+{
+	return x + y;
+}
 int main(void)
 {
 	int a = 10;
 	int b = 20;
-	int sum = a + b;
+	int sum = add(a,b);
 	printf("sum = %d\n",sum);
 	printf("Learing git and github!\n");
 	return 0;
