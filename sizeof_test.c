b0e4e6aecf6cbccfb592267116d3d9d3759f1a2c
sola2209232009-source <sola2209232009@gmail.com>
2026-09-02 20:39:30 +0800

    Create sizeof_test.c

---
+++ sizeof_test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    size_t size_char = sizeof(char);
+    size_t size_short = sizeof(short);
+    size_t size_int = sizeof(int);
+    size_t size_long = sizeof(long);
+    size_t size_char_ptr =sizeof(char *);
+    printf("char: %zu bytes\n",size_char);
+    printf("short: %zu bytes\n",size_short);
+    printf("int: %zu bytes\n",size_int);
+    printf("long: %zu bytes\n",size_long);
+    printf("char *: %zu bytes\n",size_char_ptr);
+    return 0;
+}
