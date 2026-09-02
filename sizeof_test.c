ea039b871135275fae67ecb5d2a8a6efbd8d042e
sola2209232009-source <sola2209232009@gmail.com>
2026-09-02 20:41:13 +0800

    Update sizeof_test.c

--- sizeof_test.c
+++ sizeof_test.c
@@ -5,11 +5,13 @@ int main(void)
     size_t size_short = sizeof(short);
     size_t size_int = sizeof(int);
     size_t size_long = sizeof(long);
-    size_t size_char_ptr =sizeof(char *);
+    size_t size_char_ptr = sizeof(char *);
+    size_t size_int_prt = sizeof(int *);
     printf("char: %zu bytes\n",size_char);
     printf("short: %zu bytes\n",size_short);
     printf("int: %zu bytes\n",size_int);
     printf("long: %zu bytes\n",size_long);
     printf("char *: %zu bytes\n",size_char_ptr);
+    printf("int *: %zu bytes\n",size_int_prt);
     return 0;
 }
